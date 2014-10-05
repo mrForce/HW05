277a2bba136578aa959ddafd759111654fba26e0
Jordan Force <jordan.force@uconn.edu>
2014-10-05 13:14:28 -0400

    Forgot to change something minor

--- radius_of_triangle.c
+++ radius_of_triangle.c
@@ -3,13 +3,14 @@
 #include "triangle.h"
 int main(void){
 	struct point2d a = {2.0, 2.0};
-	struct point2d b = {4.0, 4.0};
-	struct point2d c = {8.0, 8.0};
+	struct point2d b = {-2.0, 1.0};
+	struct point2d c = {0.0, -3.0};
 	struct triangle tt = {a, b, c};
 	double ar = area(tt);
-	printf("area, %f \n", ar);
+	printf("area: %f \n", ar);
 	double per = perimeter(tt);
+	printf("perimeter: %f \n", per);
 	double radius = 2.0*ar/per;
-	printf("%f ", radius);
+	printf("radius: %f \n", radius);
 	return 0;
 }
